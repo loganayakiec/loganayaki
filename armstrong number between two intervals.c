e4bf8c61c541172940deeb39755a9d0f4cf37ec9
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-01-21 10:34:49 +0530

    Create armstrong number between two intervals.c

---
+++ armstrong number between two intervals.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+int low,high,i,temp1,temp2,remainder,n=0,result=0;
+printf("enter two numbers(intervals):");
+scanf("%d%d",&low&high);
+printf("armstrong numbers between %d and %d are:",low,high);
+for(i=low+1;i<high;++1)
+{
+temp 2=i;
+temp 1=i;
+while(temp 1!=0)
+{
+temp 1/=10;
+++n;
+}
+while(temp 2!=0)
+{
+remainter=temp 2%10;
+result+=pow(remainter,n);
+temp 2/=10;
+}
+if(result==i)
+}
+if(result==i)
+{
+printf("d",i);
+}
+n=0;
+result=0;
+}
+result =;
+}
