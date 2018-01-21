f6aa7009da5e1cb1938d71fa84195724415cfc7a
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-01-21 11:21:27 +0530

    Create first five multiples.c

---
+++ first five multiples.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,i,f;
+printf("enter the numbers");
+scanf("%d",&n);
+for(i=0;i<=0;i++)
+{
+f=n*i;
+printf("the first five multiples of number is %d",f);
+}
+return 0;
+}
