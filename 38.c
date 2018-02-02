75dd43f8016a9273b5ba4595927df228aa12767e
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-02 15:42:28 +0530

    Create 38.c

---
+++ 38.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i=50;
+k=90;
+printf("value of i=%d k=%d before swap",i,k);
+i=i^k;
+k=i^k;
+i=i^k;
+printf("value of i=%d k=%d after swap",i,k);
+return 0;
+}
