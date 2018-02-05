8bdd885ff24fbd49238822cafb15ab108b033236
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-05 15:42:38 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<stdio.h>
+int main()
+{
+int i,j=0;
+char a[100];
+count<<"enter the string";
+cin>>a[100];
+for(i=0;i<100;i++)
+{
+is digit (a[i])
+++j;
+}
+cout<<"num of numeric %d"<<j;
+getch();
+}
