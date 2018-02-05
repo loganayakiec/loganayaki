acf6a40033f449caba5ba5f60367b89380545e15
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-05 16:18:01 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str1[s1],str2[s2],i,j;
+printf("\n enter the first string");
+scanf("%s",str1);
+printf("\n enter the second string");
+scanf("%s",str2);
+int i=strlen(s1);
+int j=strlen(s2);
+if(i==j)
+{
+printf("string is equal");
+}
+if(i>j)
+{
+printf("str one is greater");
+}
+else
+{
+printf("str two is greater");
+}
+}
