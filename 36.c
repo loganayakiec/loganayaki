1dfe651d5fc4c58fe6cde6f3d80fc56e4aad54f0
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-05 15:47:57 +0530

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<stdio.h>
+int main()
+{
+int i,j=0;
+char a[100];
+cout<<"enter the string";
+cin>>a[100];
+for(i=0;i<100;i++)
+{
+is digit (a[100])
+n=0;
+else
+++n;
+}
+cout<<"num of numeric %d"<<j;
+getch();
+}
