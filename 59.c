fb288697c959a677e43f4c73200724683c23b287
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-07 16:07:30 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[10],i,n,j;
+for(i=0;i<10;i++)
+{
+printf("enter the number %d",i);
+scanf("%d",a[10]);
+}
+n=a[0];
+for(j=0;j<10;j++)
+{
+if(n<a[j])
+n=a[j];
+}
+printf("the largest number is %d",n);
+return 0;
+}
