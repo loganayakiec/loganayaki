74824523a100a0374939c8b77a5cd1596a59359c
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-09 16:21:45 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int l,i,c;
+char a[20]="fancy 726";
+l=strlen(a);
+for(i=1;i<=1;i++)
+{
+while(((a[i]=='a')||(a[i]=='A'))&&((a[i]=='z')||(a[i]=='Z')))
+{
+c++;
+}
+}
+if(c==1)
+{
+printf("%s is alphabet,a);
+}
+else
+{
+printf("%s is alphabet and numeric,a");
+}
+}
