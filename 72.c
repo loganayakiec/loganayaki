77a9138c027ab1f7a026d539bdfcf94600f9f75e
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-12 16:39:34 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,12 @@
+#include<stddio.h>
+int main()
+{
+int ch;
+printf("enter the characters");
+scanf("%d",&ch);
+if(ch=='a'//ch=='e'//ch=='i'//ch=='o'//ch=='u')
+printf("the character is vowel");
+else
+printf("the character is consonant");
+return 0;
+}
