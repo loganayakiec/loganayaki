6f73df9f5a3918802911cd3d06228c2e311b374d
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-21 16:17:06 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,c=0,x=1,p=2,i;
+scanf("%d",&n);
+while(n!==0)
+{
+n=n/2;
+c++;
+}
+for(i=0;i<c+1;i++)
+{
+x=x*d;
+printf("the nearest greater power of 2 is:%d",x);
+}
+}
