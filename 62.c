2689443a5ae0093fd4a16f075bc79edd48b237e4
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-21 16:05:19 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int j,flag=0;
+char a[8]="10111001";
+for(j=0;j<=10;j++)
+{
+if((a[i]==0)||(a[i]==1))
+{
+flag++;
+}
+}
+if(flag==4)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
