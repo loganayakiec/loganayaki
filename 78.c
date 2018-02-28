bb5da51e7c048c76b55388ebc2a3a5cf0d1a3a0e
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-28 16:38:53 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+return 0;
+}
