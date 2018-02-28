651c72fa321e0a45376c8eac8302170a5ae6c5b2
loganayakiec <35220250+loganayakiec@users.noreply.github.com>
2018-02-28 15:55:51 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int ans;
+float no;
+printf("enter the decimal");
+scanf("%f",&no);
+ans=no+1;
+printf("the rounted value is %d",ans");
+return 0;
+}
